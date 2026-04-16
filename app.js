12dd72e2c523a6428929470db01e07771eb666ca
vedu <patilvedant9112@gmail.com>
2026-04-17 01:00:19 +0530

    final test

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from AWS DevOps Project ");
+//   res.end("Hello from AWS DevOps Project ");
+res.end("FINAL PROJECT DONE 🚀🔥");
 });
 
 server.listen(3000, () => {
