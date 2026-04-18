a1b76a5beb24bce4d34e6e56775f8962e976801b
vedu <patilvedant9112@gmail.com>
2026-04-18 12:51:55 +0530

    final 6

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
 
 //   res.end("Hello from AWS DevOps Project ");
-res.end("vvvvvvv");
+res.end("hello world");
 });
 
 server.listen(3000, () => {
